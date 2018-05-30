f12fb656df410b4e59d56a4e7109d97b12a95f4e
Andrew Chamamme <mastys100@gmail.com>
2018-05-30 12:46:22 +0000

    Name changed

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'GHANA REGISTRY'),
+    'name' => env('APP_NAME', 'CIVIL REGISTRY'),
 
     /*
     |--------------------------------------------------------------------------
